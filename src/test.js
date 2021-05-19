b664e4a2d4bb78bb41f147409b6686b4762c453f
unknown <aniakuchibuike@gmail.com>
2021-05-20 00:39:49 +0100

    refactoring

--- src/test.js
+++ src/test.js
@@ -1,10 +1,12 @@
-// import dialog from './helper'
+
+
 
 describe('ATOMATION OF BILLABLE HOUR MOBILE APP', () => {
 
     it("should get Company details", () => {
-        const field = $("android.widget.EditText");
-        field.setValue("/storage/emulated/0/sample2.csv");
+        const field = $("//android.widget.EditText[@index='2']")
+        field.waitForDisplayed({timeout: 9000});
+        field.addValue("/storage/emulated/0/sample2.csv'");
         const conButton = $('android.view.View[@index="3"]');
         conButton.click()
         const getDetail = $('android.view.View[@index="5"]');
