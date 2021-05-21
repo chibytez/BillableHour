9468ad1b7b61b6485f5355e5c834cd8c7de896d0
unknown <aniakuchibuike@gmail.com>
2021-05-21 15:13:22 +0100

    restructure

--- src/test.js
+++ src/test.js
@@ -6,7 +6,7 @@ describe('ATOMATION OF BILLABLE HOUR MOBILE APP', () => {
     it("should get Company details", () => {
         const field = $("//android.widget.EditText[@index='2']")
         field.waitForDisplayed({timeout: 9000});
-        field.addValue("/storage/emulated/0/sample2.csv'");
+        field.addValue("/storage/emulated/0/sample2.csv");
         const conButton = $('android.view.View[@index="3"]');
         conButton.click()
         const getDetail = $('android.view.View[@index="5"]');
